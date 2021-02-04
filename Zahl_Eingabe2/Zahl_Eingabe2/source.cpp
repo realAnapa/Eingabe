de351de6db2979465504450bdceb8ae6d664ccf9
realAnapa <haluk.anapa@googlemail.com>
2021-02-04 14:54:46 +0100

    Test Eingabe

---
+++ Zahl_Eingabe2/Zahl_Eingabe2/source.cpp
@@ -0,0 +1,27 @@
+/*
+ * source.cpp
+ *
+ *  Created on: 02.02.2021
+ *      Author: User
+ */
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int anzahl;
+	double preis;
+
+	cout << "Anzahl eingeben: ";
+	cin >> anzahl;
+
+	cout << "Preis in Euro angeben: ";
+	cin >> preis;
+
+	cout << "Anzahl: " << anzahl << endl;
+	cout << "Preis: " << preis << " Euro" << endl;
+}
+
+
